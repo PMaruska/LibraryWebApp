3590d1de051726721b2120e231f0dfd3128f47c9
Paskal Maruska <pm310865@student.polsl.pl>
2026-01-19 21:01:45 +0100

    Migracja danych - dodanie tabeli użytkownika

--- Library.Domain/AppUser.cs
+++ Library.Domain/AppUser.cs
@@ -5,6 +5,6 @@ namespace Library.Domain
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
-        public string Bio { get; set; }
+        public string? Bio { get; set; }
     }
 }
